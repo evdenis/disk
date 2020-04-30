3574d28bfbeb52070fc92621eaaafa7dc6c27648
Denis Efremov <efremov@linux.com>
2020-04-30 13:30:44 +0300

    Add gptdisk utils
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- customize.sh
+++ customize.sh
@@ -1,2 +1,6 @@
 set_perm $MODPATH/system/bin/fdisk 0 0 0755
 set_perm $MODPATH/system/bin/sfdisk 0 0 0755
+set_perm $MODPATH/system/bin/cgdisk 0 0 0755
+set_perm $MODPATH/system/bin/fixparts 0 0 0755
+set_perm $MODPATH/system/bin/gdisk 0 0 0755
+set_perm $MODPATH/system/bin/sgdisk 0 0 0755
